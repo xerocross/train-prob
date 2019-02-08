4c7deb71b95cc5d2e753ad9c0b6fb3c7c7ed7885
xerocross <nonya@business.com>
2019-02-08 01:04:15 -0500

    add some more tests

--- src/classes/digraph.test.js
+++ src/classes/digraph.test.js
@@ -7,6 +7,13 @@ var digraph_1 = __importDefault(require("./digraph"));
 var edge_1 = __importDefault(require("./edge"));
 var simple_node_1 = __importDefault(require("./simple-node"));
 // some general tests to see if digraph is working as expected.
+test("can add and get node by key", function () {
+    var nodeA = new simple_node_1.default("Atlanta");
+    var nodeB = new simple_node_1.default("Boston");
+    var path = new edge_1.default(nodeA, nodeB, 10);
+    var digraph = new digraph_1.default([path]);
+    expect(digraph.getNodeByKey("Atlanta")).toBe(nodeA);
+});
 test("internal distance matrix works on simple input", function () {
     var nodeA = new simple_node_1.default("A");
     var nodeB = new simple_node_1.default("B");
@@ -23,4 +30,84 @@ test("internal distance matrix works input with three nodes, two paths (20)", fu
     var path2 = new edge_1.default(nodeB, nodeC, 20);
     var digraph = new digraph_1.default([path1, path2]);
     expect(digraph.getEdgeWeight("B", "C")).toBe(20);
+});
+test("getRouteDistance (30)", function () {
+    var nodeA = new simple_node_1.default("A");
+    var nodeB = new simple_node_1.default("B");
+    var nodeC = new simple_node_1.default("C");
+    var path1 = new edge_1.default(nodeA, nodeB, 10);
+    var path2 = new edge_1.default(nodeB, nodeC, 20);
+    var digraph = new digraph_1.default([path1, path2]);
+    var route = digraph.buildRoute(["A", "B", "C"]);
+    expect(digraph.getRouteDistance(route)).toBe(30);
+});
+test("getRouteDistance (15)", function () {
+    var nodeA = new simple_node_1.default("A");
+    var nodeB = new simple_node_1.default("B");
+    var nodeC = new simple_node_1.default("C");
+    var nodeD = new simple_node_1.default("D");
+    var path1 = new edge_1.default(nodeA, nodeB, 5);
+    var path2 = new edge_1.default(nodeB, nodeC, 5);
+    var path3 = new edge_1.default(nodeC, nodeD, 5);
+    var digraph = new digraph_1.default([path1, path2, path3]);
+    var route = digraph.buildRoute(["A", "B", "C", "D"]);
+    expect(digraph.getRouteDistance(route)).toBe(15);
+});
+test("getRouteDistance invalid route throws", function () {
+    var nodeA = new simple_node_1.default("A");
+    var nodeB = new simple_node_1.default("B");
+    var nodeC = new simple_node_1.default("C");
+    var path1 = new edge_1.default(nodeA, nodeB, 10);
+    var path2 = new edge_1.default(nodeB, nodeC, 20);
+    var digraph = new digraph_1.default([path1, path2]);
+    var route = digraph.buildRoute(["A", "C"]);
+    expect(function () {
+        digraph.getRouteDistance(route);
+    }).toThrow();
+});
+test("getRoutesBetween (2)", function () {
+    var nodeA = new simple_node_1.default("A");
+    var nodeB = new simple_node_1.default("B");
+    var nodeC = new simple_node_1.default("C");
+    var nodeD = new simple_node_1.default("D");
+    var path1 = new edge_1.default(nodeA, nodeB, 5);
+    var path2 = new edge_1.default(nodeB, nodeC, 5);
+    var path3 = new edge_1.default(nodeA, nodeD, 5);
+    var path4 = new edge_1.default(nodeD, nodeC, 5);
+    var digraph = new digraph_1.default([path1, path2, path3, path4]);
+    var routes = digraph.getRoutesBetween("A", "C", 5);
+    expect(routes.length).toBe(2);
+});
+test("getRoutesBetween (3)", function () {
+    var nodeA = new simple_node_1.default("A");
+    var nodeB = new simple_node_1.default("B");
+    var nodeC = new simple_node_1.default("C");
+    var nodeD = new simple_node_1.default("D");
+    var nodeE = new simple_node_1.default("E");
+    var path1 = new edge_1.default(nodeA, nodeB, 5);
+    var path2 = new edge_1.default(nodeB, nodeC, 5);
+    var path3 = new edge_1.default(nodeA, nodeD, 5);
+    var path4 = new edge_1.default(nodeD, nodeC, 5);
+    var path5 = new edge_1.default(nodeA, nodeE, 5);
+    var path6 = new edge_1.default(nodeE, nodeC, 5);
+    var digraph = new digraph_1.default([path1, path2, path3, path4, path5, path6]);
+    var routes = digraph.getRoutesBetween("A", "C", 5);
+    expect(routes.length).toBe(3);
+});
+// getShortestDistance
+test("getShortestDistance", function () {
+    var nodeA = new simple_node_1.default("A");
+    var nodeB = new simple_node_1.default("B");
+    var nodeC = new simple_node_1.default("C");
+    var nodeD = new simple_node_1.default("D");
+    var nodeE = new simple_node_1.default("E");
+    var path1 = new edge_1.default(nodeA, nodeB, 20);
+    var path2 = new edge_1.default(nodeB, nodeC, 20);
+    var path3 = new edge_1.default(nodeA, nodeD, 2);
+    var path4 = new edge_1.default(nodeD, nodeC, 2);
+    var path5 = new edge_1.default(nodeA, nodeE, 5);
+    var path6 = new edge_1.default(nodeE, nodeC, 5);
+    var digraph = new digraph_1.default([path1, path2, path3, path4, path5, path6]);
+    var d = digraph.getShortestDistance("A", "C");
+    expect(d).toBe(4);
 });
--- src/classes/digraph.test.ts
+++ src/classes/digraph.test.ts
@@ -7,6 +7,14 @@ import SimpleNode from "./simple-node";
 
 // some general tests to see if digraph is working as expected.
 
+test("can add and get node by key", function () {
+    const nodeA = new SimpleNode ("Atlanta");
+    const nodeB = new SimpleNode("Boston");
+    const path = new Edge(nodeA, nodeB, 10);
+    const digraph = new Digraph([path]);
+    expect(digraph.getNodeByKey("Atlanta")).toBe(nodeA);
+});
+
 test("internal distance matrix works on simple input", function () {
     const nodeA = new SimpleNode("A");
     const nodeB = new SimpleNode("B");
@@ -24,4 +32,90 @@ test("internal distance matrix works input with three nodes, two paths (20)", fu
     const path2 = new Edge(nodeB, nodeC, 20);
     const digraph = new Digraph([path1, path2]);
     expect(digraph.getEdgeWeight("B", "C")).toBe(20);
+});
+
+test("getRouteDistance (30)", function () {
+    const nodeA = new SimpleNode("A");
+    const nodeB = new SimpleNode("B");
+    const nodeC = new SimpleNode("C");
+    const path1 = new Edge(nodeA, nodeB, 10);
+    const path2 = new Edge(nodeB, nodeC, 20);
+    const digraph = new Digraph([path1, path2]);
+    const route = digraph.buildRoute(["A", "B", "C"]);
+    expect(digraph.getRouteDistance(route)).toBe(30);
+});
+
+test("getRouteDistance (15)", function () {
+    const nodeA = new SimpleNode("A");
+    const nodeB = new SimpleNode("B");
+    const nodeC = new SimpleNode("C");
+    const nodeD = new SimpleNode("D");
+    const path1 = new Edge(nodeA, nodeB, 5);
+    const path2 = new Edge(nodeB, nodeC, 5);
+    const path3 = new Edge(nodeC, nodeD, 5);
+    const digraph = new Digraph([path1, path2, path3]);
+    const route = digraph.buildRoute(["A", "B", "C", "D"]);
+    expect(digraph.getRouteDistance(route)).toBe(15);
+});
+
+test("getRouteDistance invalid route throws", function () {
+    const nodeA = new SimpleNode("A");
+    const nodeB = new SimpleNode("B");
+    const nodeC = new SimpleNode("C");
+    const path1 = new Edge(nodeA, nodeB, 10);
+    const path2 = new Edge(nodeB, nodeC, 20);
+    const digraph = new Digraph([path1, path2]);
+    const route = digraph.buildRoute(["A", "C"]);
+    expect(function () {
+        digraph.getRouteDistance(route);
+    }).toThrow();
+});
+
+test("getRoutesBetween (2)", function () {
+    const nodeA = new SimpleNode("A");
+    const nodeB = new SimpleNode("B");
+    const nodeC = new SimpleNode("C");
+    const nodeD = new SimpleNode("D");
+    const path1 = new Edge(nodeA, nodeB, 5);
+    const path2 = new Edge(nodeB, nodeC, 5);
+    const path3 = new Edge(nodeA, nodeD, 5);
+    const path4 = new Edge(nodeD, nodeC, 5);
+    const digraph = new Digraph([path1, path2, path3, path4]);
+    const routes = digraph.getRoutesBetween("A", "C", 5);
+    expect(routes.length).toBe(2);
+});
+
+test("getRoutesBetween (3)", function () {
+    const nodeA = new SimpleNode("A");
+    const nodeB = new SimpleNode("B");
+    const nodeC = new SimpleNode("C");
+    const nodeD = new SimpleNode("D");
+    const nodeE = new SimpleNode("E");
+    const path1 = new Edge(nodeA, nodeB, 5);
+    const path2 = new Edge(nodeB, nodeC, 5);
+    const path3 = new Edge(nodeA, nodeD, 5);
+    const path4 = new Edge(nodeD, nodeC, 5);
+    const path5 = new Edge(nodeA, nodeE, 5);
+    const path6 = new Edge(nodeE, nodeC, 5);
+    const digraph = new Digraph([path1, path2, path3, path4, path5, path6]);
+    const routes = digraph.getRoutesBetween("A", "C", 5);
+    expect(routes.length).toBe(3);
+});
+
+// getShortestDistance
+test("getShortestDistance", function () {
+    const nodeA = new SimpleNode("A");
+    const nodeB = new SimpleNode("B");
+    const nodeC = new SimpleNode("C");
+    const nodeD = new SimpleNode("D");
+    const nodeE = new SimpleNode("E");
+    const path1 = new Edge(nodeA, nodeB, 20);
+    const path2 = new Edge(nodeB, nodeC, 20);
+    const path3 = new Edge(nodeA, nodeD, 2);
+    const path4 = new Edge(nodeD, nodeC, 2);
+    const path5 = new Edge(nodeA, nodeE, 5);
+    const path6 = new Edge(nodeE, nodeC, 5);
+    const digraph = new Digraph([path1, path2, path3, path4, path5, path6]);
+    const d = digraph.getShortestDistance("A", "C");
+    expect(d).toBe(4);
 });
